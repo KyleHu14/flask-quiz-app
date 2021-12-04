03a1f95daf2b532a2821923517421fca9082f122
KyleHu14 <kylebusiness1688@gmail.com>
2021-12-03 23:13:01 -0800

    First prototype

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -14,9 +14,39 @@ migrate = Migrate(app, db)
 # [END Initialization]
 
 # [START Views]
-@app.route('/admin')
+@app.route('/')
+def home():
+    return render_template('base.html', navbar_title = 'Home')
+
+@app.route('/admin', methods = ['GET', 'POST'])
 def admin():
-    return render_template('admin.html')
+    form = QuestionForm()
+    if form.validate_on_submit():
+        new_question = Question(question_text = form.question_text.data, answer_text = form.answer_text.data)
+
+        db.session.add(new_question)
+        db.session.commit()
+        print('New Question Created')
+    
+        form.question_text.data, form.answer_text.data = "", ""
+
+    return render_template('admin.html', title = 'Admin Page', form = form, navbar_title = 'Add Question')
+
+@app.route('/user', methods = ['GET', 'POST'])
+def user():
+    all_questions = Question.query.all()
+    if request.method == 'POST':
+        user_correct = 0
+        for index_id in range(1,len(all_questions)+1):
+            user_answer = request.form.get(f'answer{index_id}')
+            if user_answer == Question.query.filter_by(id = index_id).first().answer_text:
+                user_correct += 1
+            results_dict = {
+                'num_correct' : user_correct,
+                'percent_score' : float(user_correct/len(all_questions))*100
+            }
+        return render_template('user.html', questions = all_questions, navbar_title = "Questions", results = results_dict)
+    return render_template('user.html', questions = all_questions, navbar_title = "Questions")
 # [END Views]
 
 # [START Forms]
